15a2573a2209ce4a060274817bb3b34f4fce7f53
Aliya Yusuf Shaikh <aaliyashaikh4013@gmail.com>
2020-07-30 03:32:11 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -32,9 +32,9 @@ class Profile(models.Model):
 
 class Comment(models.Model):
 	post = models.ForeignKey(Post,on_delete = models.CASCADE,related_name='comments')
-	user = models.ForeignKey(User,on_delete = models.CASCADE)
+	name = models.CharField(max_length = 255, null = True)
 	body = models.TextField(blank=True,null=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
-		return '{}-{}'.format(self.post.title, str(self.user.username))
+		return '{}-{}'.format(self.post.title, self.name)
