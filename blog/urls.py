770c420b1ab1f8fa7d2668d54e08a4bf7f5f6ea9
Aliya Yusuf Shaikh <aaliyashaikh4013@gmail.com>
2020-07-30 03:31:24 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views as user_views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import PostDetailView,PostCreateView,PostUpdateView,PostDeleteView,PostListView,UserPostListView
+from .views import PostCreateView,CommentCreateView,PostUpdateView,PostDeleteView,PostListView,UserPostListView,PostDetailView
 
 urlpatterns = [
     path('',PostListView.as_view(),name='home'),
@@ -14,7 +14,7 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='blog/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='blog/logout.html'),name='logout'),
     path('profile/',user_views.profile,name='profile'),
-    path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/new/',PostCreateView.as_view(),name='post-create'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
     path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
@@ -31,6 +31,7 @@ urlpatterns = [
     path('privacy/',views.privacy,name='privacy'),
     path('terms/',views.terms,name='terms'),
     path('me/',views.me,name='me'),
+    path('post/<int:pk>/comment/',CommentCreateView.as_view(),name='add_comment'),
 ]
 
 
